2bb55d890e756c6c0122c11243a41dba3ad54ac5
aswinlakshmanan-s <sriram.a@northeastern.edu>
2025-06-25 19:14:46 -0400

    LLD Design for Parking Garage Problem

---
+++ Parking.py
@@ -0,0 +1,102 @@
+from enum import Enum
+class VehicleType(Enum):
+    CAR = 1
+    MOTORCYCLE = 2
+    TRUCK = 3
+
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    def __init__(self, license_plate: str, vehicle_type: VehicleType):
+        self.license_plate = license_plate
+        self.type = vehicle_type
+
+#each of below class imports same things
+class Truck(Vehicle):
+    def __init__(self, license_plate: str):
+        super().__init__(license_plate, VehicleType.TRUCK)
+
+class Car(Vehicle):
+    def __init__(self, license_plate: str):
+        super().__init__(license_plate, VehicleType.CAR)
+
+class Motorcycle(Vehicle):
+    def __init__(self, license_plate: str):
+        super().__init__(license_plate, VehicleType.MOTORCYCLE)
+
+
+class ParkingSpot(Vehicle, VehicleType):
+    def __init__(self, spot_number: int):
+        self.spot_number = spot_number
+        self.vehicle_type = VehicleType.CAR  # Default vehicle type is CAR
+        self.parked_vehicle = None
+
+    def is_available(self) -> bool:
+        return self.parked_vehicle is None
+
+    #add vehicle to parking spot
+    #check if vehicle is the right type for the spot
+    def park_vehicle(self, vehicle: Vehicle) -> None:
+        if self.is_available() and vehicle.get_type() == self.vehicle_type:
+            self.parked_vehicle = vehicle
+        else:
+            raise ValueError("Invalid vehicle type or spot already occupied.")
+
+    def unpark_vehicle(self) -> None:
+        self.parked_vehicle = None
+
+    def get_vehicle_type(self) -> VehicleType:
+        return self.vehicle_type
+
+    def get_parked_vehicle(self) -> Vehicle:
+        return self.parked_vehicle
+
+from typing import List
+
+class Level(ParkingSpot, Vehicle):
+    def __init__(self, floor: int, num_spots: int):
+        self.floor = floor
+        self.parking_spots: List[ParkingSpot] = [ParkingSpot(i) for i in range(num_spots)]
+
+    def park_vehicle(self, vehicle: Vehicle) -> bool:
+        for spot in self.parking_spots:
+            if spot.is_available() and spot.get_vehicle_type() == vehicle.get_type():
+                spot.park_vehicle(vehicle)
+                return True
+        return False
+
+    def unpark_vehicle(self, vehicle: Vehicle) -> bool:
+        for spot in self.parking_spots:
+            if not spot.is_available() and spot.get_parked_vehicle() == vehicle:
+                spot.unpark_vehicle()
+                return True
+        return False
+    
+from typing import List
+
+class ParkingLot(Level, Vehicle):
+    _instance = None
+
+    def __init__(self):
+        if ParkingLot._instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            ParkingLot._instance = self
+            self.levels: List[Level] = []
+
+    @staticmethod
+    def get_instance():
+        if ParkingLot._instance is None:
+            ParkingLot()
+        return ParkingLot._instance
+
+    def park_vehicle(self, vehicle: Vehicle) -> bool:
+        for level in self.levels:
+            if level.park_vehicle(vehicle):
+                return True
+        return False
+
+    def unpark_vehicle(self, vehicle: Vehicle) -> bool:
+        for level in self.levels:
+            if level.unpark_vehicle(vehicle):
+                return True
+        return False
